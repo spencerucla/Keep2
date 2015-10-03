ad70c201d6ca6a4bb15bbe5ac942ba9a103dc296
Spencer Sutterlin <spencerucla@gmail.com>
2015-10-03 12:31:42 -0700

    Change action bar to toolbar

--- app/src/main/java/com/example/spencers/keep2/MainActivity.java
+++ app/src/main/java/com/example/spencers/keep2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.spencers.keep2;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 
     @Override
