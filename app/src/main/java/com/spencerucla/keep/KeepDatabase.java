2228c781f41c95e4d52eb3cfacc367ed4453ce0d
Spencer Sutterlin <spencerucla@gmail.com>
2022-03-24 12:54:25 -0700

    Database: fix spacing lint

--- app/src/main/java/com/spencerucla/keep/KeepDatabase.java
+++ app/src/main/java/com/spencerucla/keep/KeepDatabase.java
@@ -12,21 +12,21 @@ import com.spencerucla.keep.model.Todo;
 @Database(entities = { Note.class, Todo.class }, version = 1)
 public abstract class KeepDatabase extends RoomDatabase {
 
-  public abstract TodoListDao todoListDao();
+    public abstract TodoListDao todoListDao();
 
-  private static KeepDatabase sInstance;
+    private static KeepDatabase sInstance;
 
-  static KeepDatabase getDatabase(Context context) {
-    if (sInstance == null) {
-      synchronized (KeepDatabase.class) {
+    static KeepDatabase getDatabase(Context context) {
         if (sInstance == null) {
-          sInstance = Room.databaseBuilder(context.getApplicationContext(),
-                  KeepDatabase.class, "keep_database")
-                  .fallbackToDestructiveMigration()
-                  .build();
+            synchronized (KeepDatabase.class) {
+                if (sInstance == null) {
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            KeepDatabase.class, "keep_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
         }
-      }
+        return sInstance;
     }
-    return sInstance;
-  }
 }
